4e136f51873519bf4272349e4c7e6e3cb2253293
elkyre <43600752+elkyre@users.noreply.github.com>
2018-11-07 19:33:38 +1100

    added hero.png, collison.cs

--- Inverse/Inverse/Player.cs
+++ Inverse/Inverse/Player.cs
@@ -9,7 +9,7 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace PLATFORMER1
+namespace Inverse
 {
     public class Player
     {
