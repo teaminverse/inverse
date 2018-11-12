80b454336d4d34c1a8d079cd397e85406661e968
Skullatoon <luke.Bowler@hotmail.com>
2018-11-12 21:57:31 +1100

    Collision class
    basic collisions
    cut out unnecessary info

--- Inverse/Inverse/Collisions.cs
+++ Inverse/Inverse/Collisions.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
 
 namespace Inverse
 {
     class Collision
+   
     {
-
     }
 }
