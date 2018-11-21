a453321e6b7a692d778fa3ae79788190c88ec913
Skullatoon <luke.Bowler@hotmail.com>
2018-11-21 20:19:11 +1100

    grav
    gravity for above and below
    updated player class

---
+++ Inverse/Inverse/temp/Player.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Inverse
+{
+    public class Player
+    {
+        public Sprite playerSprite = new Sprite();
+        public bool gravDown = true;
+        Game1 game = null;
+        public float jumpStrength = 25000f;
+
+        Collisions collision = new Collisions();
+
+        public Player()
+        {
+
+        }
+
+        public void Load(ContentManager content, Game1 theGame)
+        {
+            playerSprite.Load(content, "hero", true);
+
+            game = theGame;
+            playerSprite.velocity = Vector2.Zero;
+            playerSprite.position = new Vector2(50, 150);
+        }
+
+        public void Update(float deltaTime)
+        {
+            UpdateInput(deltaTime);
+            playerSprite.Update(deltaTime);
+            playerSprite.UpdateHitBox();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            playerSprite.Draw(spriteBatch, game);
+        }
+
+        private void UpdateInput(float deltaTime)
+        {
+            if(gravDown == true)
+            {
+
+                playerSprite = collision.CollideBelow(playerSprite, game.platform.platformSprite, deltaTime);
+
+                Vector2 localAcceleration = game.gravity;
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Space) && playerSprite.canJump == true)
+                {
+                    playerSprite.canJump = false;
+                    localAcceleration.Y -= jumpStrength;
+                }
+
+                playerSprite.velocity += localAcceleration * deltaTime;
+                playerSprite.position += playerSprite.velocity * deltaTime;
+
+
+
+            }
+            else
+            {
+                playerSprite = collision.CollideAbove(playerSprite, game.platform.platformSprite, deltaTime);
+                Vector2 localAcceleration = -game.gravity;
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Space) && playerSprite.canJump == true)
+
+                {
+                    playerSprite.canJump = false;
+                    localAcceleration.Y += jumpStrength;
+
+                }
+
+                playerSprite.velocity += localAcceleration * deltaTime;
+                playerSprite.position += playerSprite.velocity * deltaTime;
+
+            }
+
+        }
+    }
+}
