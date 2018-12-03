b9ac00258aaeeb1380055d1a973a15e1984bf31a
Skullatoon <luke.Bowler@hotmail.com>
2018-12-03 19:22:06 +1100

    bg maingame push
    bg

--- Inverse/Inverse/Background.cs
+++ Inverse/Inverse/Background.cs
@@ -9,35 +9,55 @@ using System.Threading.Tasks;
 
 namespace Inverse
 {
-
-
     class Background
     {
-
-
-        public Sprite BGscroll = new Sprite();
-
+        public Sprite background = new Sprite();
+        public Sprite paraBG = new Sprite();
         MainGame game = null;
-        float scrollMove = -40;
-
+        float scrollMove = -40f;
+        float scrollMove2 = -60f;
         public void Load(ContentManager content, MainGame theGame)
         {
+            background.Load(content, "BGflip", false);
+            paraBG.Load(content, "BGflip2", false);
             game = theGame;
 
-            BGscroll.Load(content, "BGflip", false);
+            AnimatedTexture bganimation = new AnimatedTexture(Vector2.Zero, 0, 1, 1);
+            bganimation.Load(content, "BGflip", 1, 0);
+            background.AddAnimation(bganimation, 0, 1);
+            background.Pause();
 
-            AnimatedTexture animation = new AnimatedTexture(Vector2.Zero, 0, 1, 1);
-            animation.Load(content, "BGflip", 1, 1);
-            BGscroll.AddAnimation(animation, 0, 0);
-            BGscroll.Pause();
+            AnimatedTexture paraanimation = new AnimatedTexture(Vector2.Zero, 0, 1, 1);
+            paraanimation.Load(content, "BGflip2", 1, 0);
+            paraBG.AddAnimation(paraanimation, 0, 1);
+            paraBG.Pause();
+            background.velocity = Vector2.Zero;
+            background.position = new Vector2(0, 0);
+            paraBG.velocity = Vector2.Zero;
+            paraBG.position = new Vector2(0, 0);
         }
-        public void Draw(SpriteBatch spriteBatch)
+        public void Update(float deltaTime)
         {
-            BGscroll.Draw(spriteBatch, game);
+            background.velocity = new Vector2(scrollMove, 0);
+            paraBG.velocity = new Vector2(scrollMove2, 0);
+            background.position += background.velocity * deltaTime;
+            paraBG.position += paraBG.velocity * deltaTime;
+            background.Update(deltaTime);
+            paraBG.Update(deltaTime);
+            if (background.position.X < -737)
+            {
+                background.position.X = 736;
+            }
+            if (paraBG.position.X < -737)
+            {
+                paraBG.position.X = 736;
+           }
+
         }
-        public void Update(float deltaTime);
+        public void Draw(SpriteBatch spriteBatch,MainGame game)
         {
-        
+            background.Draw(spriteBatch,game);
+            paraBG.Draw(spriteBatch, game);
         }
 
 
